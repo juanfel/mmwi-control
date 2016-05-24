0715c4e43788a6dececc4561b2372e714229ad2e
juanfel <jf3dsaber@gmail.com>
2016-05-24 00:04:34 -0400

    Corregidos todos los typos del control remoto básico
    Ahora todo debería funcionar correctamente

--- funcs.el
+++ funcs.el
@@ -1,31 +1,31 @@
 (defun mmwi-control/VolumeUp ()
   "Función para escoger aleatoriamente una canción"
   (interactive)
-  (shell-command (concat mmwi-location "/Control/VolumeUp"))
+  (url-retrieve-synchronously (concat mmwi-location "/Control/VolumeUp"))
   )
 (defun mmwi-control/VolumeDown ()
   "Función para escoger aleatoriamente una canción"
   (interactive)
-  (shell-command (concat mmwi-location "/Control/VolumeDown"))
+  (url-retrieve-synchronously (concat mmwi-location "/Control/VolumeDown"))
   )
 
 (defun mmwi-control/PlayPause ()
   "Función para parar o comenzar una canción"
   (interactive)
-  (shell-command (concat mmwi-location "/Control/PlayPause"))
+  (url-retrieve-synchronously (concat mmwi-location "/Control/PlayPause"))
   )
 (defun mmwi-control/Stop ()
   "Función para parar o comenzar una canción"
   (interactive)
-  (shell-command (concat mmwi-location "/Control/Stop"))
+  (url-retrieve-synchronously (concat mmwi-location "/Control/Stop"))
   )
 (defun mmwi-control/SongPrev ()
   "Función para parar o comenzar una canción"
   (interactive)
-  (shell-command (concat mmwi-location "/Control/SongPrev"))
+  (url-retrieve-synchronously (concat mmwi-location "/Control/SongPrev"))
   )
 (defun mmwi-control/SongNext ()
   "Función para parar o comenzar una canción"
   (interactive)
-  (shell-command (concat mmwi-location "/Control/SongNext"))
+  (url-retrieve-synchronously (concat mmwi-location "/Control/SongNext"))
   )
