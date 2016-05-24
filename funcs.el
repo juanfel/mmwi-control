63f9ab193b2d774a404ec3797c4643cd4ab90cec
juanfel <jf3dsaber@gmail.com>
2016-05-23 23:59:17 -0400

    Corregidos otros typos, para darle consistencia

--- funcs.el
+++ funcs.el
@@ -1,30 +1,30 @@
-(defun mmwi/VolumeUp ()
+(defun mmwi-control/VolumeUp ()
   "Función para escoger aleatoriamente una canción"
   (interactive)
   (shell-command (concat mmwi-location "/Control/VolumeUp"))
   )
-(defun mmwi/VolumeDown ()
+(defun mmwi-control/VolumeDown ()
   "Función para escoger aleatoriamente una canción"
   (interactive)
   (shell-command (concat mmwi-location "/Control/VolumeDown"))
   )
 
-(defun mmwi/PlayPause ()
+(defun mmwi-control/PlayPause ()
   "Función para parar o comenzar una canción"
   (interactive)
   (shell-command (concat mmwi-location "/Control/PlayPause"))
   )
-(defun mmwi/Stop ()
+(defun mmwi-control/Stop ()
   "Función para parar o comenzar una canción"
   (interactive)
   (shell-command (concat mmwi-location "/Control/Stop"))
   )
-(defun mmwi/SongPrev ()
+(defun mmwi-control/SongPrev ()
   "Función para parar o comenzar una canción"
   (interactive)
   (shell-command (concat mmwi-location "/Control/SongPrev"))
   )
-(defun mmwi/SongNext ()
+(defun mmwi-control/SongNext ()
   "Función para parar o comenzar una canción"
   (interactive)
   (shell-command (concat mmwi-location "/Control/SongNext"))
--- keybindings.el
+++ keybindings.el
@@ -3,21 +3,21 @@
   :title "Foobar 2000 remote control"
   :doc "\n[_h_] Prev \[_l_] next \[_j_] play/pause \ [_i_]VolDown \[_o_] VolUp."
   :bindings
-  ("j" mmwi/PlayPause)
-  ("l" mmwi/SongNext)
-  ("h" mmwi/SongPrev)
-  ("o" mmwi/VolumeUp)
-  ("i" mmwi/VolumeDown)
+  ("j" mmwi-control/PlayPause)
+  ("l" mmwi-control/SongNext)
+  ("h" mmwi-control/SongPrev)
+  ("o" mmwi-control/VolumeUp)
+  ("i" mmwi-control/VolumeDown)
   ("0" spacemacs//transient-state-0))
 (spacemacs/declare-prefix "o" "Others")
 (spacemacs/declare-prefix "oF" "Mmwi-sin-transient")
 (spacemacs/set-leader-keys
   "of" 'spacemacs/mmwi-control-transient-state/body
-  "oFj" mmwi/PlayPause
-  "oFl" mmwi/SongNext
-  "oFh" mmwi/SongPrev
-  "oFo" mmwi/VolumeUp
-  "oFi" mmwi/VolumeDown
+  "oFj" mmwi-control/PlayPause
+  "oFl" mmwi-control/SongNext
+  "oFh" mmwi-control/SongPrev
+  "oFo" mmwi-control/VolumeUp
+  ("oFi" mmwi-control/VolumeDown)
 
   )
 
