6bdf6a220bd5dc38baff75ef98576825cecae60c
juanfel <jf3dsaber@gmail.com>
2016-05-23 23:50:45 -0400

    Cambio al formato que muestra en el transient

--- keybindings.el
+++ keybindings.el
@@ -1,7 +1,7 @@
 ;;Archivo que tiene los keybindings para el modo y toda esa caca.
 (spacemacs|define-transient-state mmwi-control
   :title "Foobar 2000 remote control"
-  :doc "\n[_h_] Prev \[_l_] next \[_j_] play/pause \ [_k_]stop \[_u_]random."
+  :doc "\n[_h_] Prev \[_l_] next \[_j_] play/pause \ [_i_]VolDown \[_o_] VolUp."
   :bindings
   ("j" mmwi/PlayPause)
   ("l" mmwi/SongNext)
