09b30c754e5d64783eb05de5f04a2ead92afda4f
juanfel <jf3dsaber@gmail.com>
2016-05-29 20:59:43 -0400

    Creada lista de comandos en config.el
    Todavía no puede ser procesada.

--- config.el
+++ config.el
@@ -1,2 +1,9 @@
 ;;Archivo con las configuraciones del layer. En particular, donde *** está mmwi
 (defvar mmwi-location "http://127.0.0.1:8080")
+(defvar mmwi-command-list '(
+      '(:command-name "VolumeUp" :command "/Control/VolumeUp" :key "o")
+      '(:command-name "VolumeDown" :command "/Control/VolumeDown" :key "i")
+      '(:command-name "PlayPause" :command "/Control/PlayPause" :key "j")
+      '(:command-name "SongPrev" :command "/Control/SongPrev" :key "h")
+      '(:command-name "SongNext" :command "/Control/SongNext" :key "l")
+      ))
